d609ddcf948496aaa1b91fca6df62e1af23c95b9
Arnab Samanta <loveumearnab.2812000@gmail.com>
2024-10-24 13:17:19 +0530

    fixed the delte videos cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,9 +17,9 @@ const uploadOnCloudinary = async (localfilepath) => {
     });
 
     //filehas been uploaded succesfully
-    console.log("file is uploaded on cloudinary succesfull", response.url);
+    console.log("file is uploaded on cloudinary succesfull", response);
     fs.unlinkSync(localfilepath); //removing the local file path as the file is uploaded succesfully
-    return response.url;
+    return response;
   } catch (error) {
     fs.unlinkSync(localfilepath);
     //remove the local file path as the file upload has failed
@@ -28,6 +28,8 @@ const uploadOnCloudinary = async (localfilepath) => {
   }
 };
 const deleteOnCloudinary = async (cloudinaryUrl, mediatype = "image") => {
+  // It is working in cloudinary  with public id not with url
+  //We need to use public id for videos with media type videos and image we can use url
   try {
     if (!cloudinaryUrl) return null;
 
@@ -36,10 +38,21 @@ const deleteOnCloudinary = async (cloudinaryUrl, mediatype = "image") => {
     const returnobject = await cloudinary.uploader.destroy(cloudinaryUrl, {
       resource_type: `${mediatype}`,
     });
+    console.log(returnobject);
+
+    if (returnobject.result === "ok") {
+      console.log("video deleted succesfully from cloudinary");
+    } else {
+      console.log("failed to delete video ", returnobject);
+    }
   } catch (err) {
-    console.log("Something went wrong while deleting the file on cloudinary");
+    console.log(
+      "Something went wrong while deleting the file on cloudinary ",
+      err
+    );
   }
 };
+
 export { uploadOnCloudinary, deleteOnCloudinary };
 
 /*cloudinary.v2.uploader.upload(
